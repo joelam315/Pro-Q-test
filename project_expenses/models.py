2500751e7b77693a45bed7728e77d1f1e23ac260
joelam315 <58076302+joelam315@users.noreply.github.com>
2024-12-03 20:59:02 +0800

    Update models.py

--- project_expenses/models.py
+++ project_expenses/models.py
@@ -5,7 +5,7 @@ import datetime
 from django.db import models
 from django.dispatch import receiver
 from projects.models import Project
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from common.fields import EncryptedImageField
 from common.constants import FETCH_URL_NAME
 
