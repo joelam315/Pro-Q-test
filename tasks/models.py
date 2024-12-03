f28a08e4ffa95144eed1ba17cba54e0ec2e51f1c
joelam315 <58076302+joelam315@users.noreply.github.com>
2024-12-03 21:01:11 +0800

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from common.models import User
 from companies.models import Company
 from contacts.models import Contact
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from teams.models import Teams
 
 
