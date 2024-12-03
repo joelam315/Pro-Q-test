10f34fc38afccfc1613541d09d48625988970361
joelam315 <58076302+joelam315@users.noreply.github.com>
2024-12-03 23:01:03 +0800

    Update urls.py

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import re_path as url
 from django.urls import path
 from contacts.views import (
     ContactsListView, CreateContactView, ContactDetailView,
