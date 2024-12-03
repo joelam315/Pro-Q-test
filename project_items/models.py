54511aff7c4cfe0c3e7968a92df3fe576a576567
joelam315 <58076302+joelam315@users.noreply.github.com>
2024-12-03 20:59:19 +0800

    Update models.py

--- project_items/models.py
+++ project_items/models.py
@@ -1,6 +1,6 @@
 import arrow
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from common.models import User
 from project_items.utils import PROJECT_TYPE, PROJECT_STATUS
 from projects.models import Project
